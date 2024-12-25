bf69075f04a7ce76680c4329efaee717f672b6a1
m-zhiltsova <m.zhiltsova@g.nsu.ru>
2024-12-26 04:24:29 +0700

    Delete task4 directory

--- task4/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wake/Documents/nsu/cpp/cpp_tasks/task4
-# Build directory: /home/wake/Documents/nsu/cpp/cpp_tasks/task4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CSVParserTest "/home/wake/Documents/nsu/cpp/cpp_tasks/task4/build/runTests")
-set_tests_properties(CSVParserTest PROPERTIES  _BACKTRACE_TRIPLES "/home/wake/Documents/nsu/cpp/cpp_tasks/task4/CMakeLists.txt;21;add_test;/home/wake/Documents/nsu/cpp/cpp_tasks/task4/CMakeLists.txt;0;")
